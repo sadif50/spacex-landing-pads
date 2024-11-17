6cd775f113275dc56f9bbc94a9f0594743831177
S. M. Sujan <smsujon50@gmail.com>
2024-11-17 19:09:52 +0600

    Added header with spacex black logo in layout

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,3 @@
+<nav class="py-[21px] flex justify-center drop-shadow bg-white">
+	<img src="/SpaceX_logo_black.svg" alt="SpaceX logo black.">
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '../app.css';
+	import Nav from '$lib/components/Nav.svelte';
 	let { children } = $props();
 </script>
 
+<Nav />
 {@render children()}
