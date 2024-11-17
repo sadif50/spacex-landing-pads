6aec427c38f6385938b7cf0fad11bbae03934407
S. M. Sujan <smsujon50@gmail.com>
2024-11-18 00:46:56 +0600

    data table will show landpads data

--- src/lib/components/dataTable/DataTable.svelte
+++ src/lib/components/dataTable/DataTable.svelte
@@ -1,8 +1,36 @@
 <script>
 	import { getContext } from 'svelte';
+  import { Button, Progressbar, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+  import {LinkOutline} from 'flowbite-svelte-icons'
 
 	const landpads = getContext('landpads');
-  console.log(landpads);
+  console.log($landpads);
 </script>
 
-<pre>{landpads}</pre>
+<Table hoverable={true}>
+  <TableHead>
+    <TableHeadCell>FULL NAME</TableHeadCell>
+    <TableHeadCell>LOCATION NAME</TableHeadCell>
+    <TableHeadCell>REGION</TableHeadCell>
+    <TableHeadCell>DETAILS</TableHeadCell>
+    <TableHeadCell>SUCCESS RATE</TableHeadCell>
+    <TableHeadCell>WIKIPEDIA LINK</TableHeadCell>
+    <TableHeadCell>STATUS</TableHeadCell>
+  </TableHead>
+  <TableBody tableBodyClass="divide-y">
+    {#each $landpads as landpad, index}
+      <TableBodyRow>
+        <TableBodyCell>{landpad.full_name}</TableBodyCell>
+        <TableBodyCell>{landpad.location.name}</TableBodyCell>
+        <TableBodyCell>{landpad.location.region}</TableBodyCell>
+        <TableBodyCell><Button>See Details</Button></TableBodyCell>
+        <TableBodyCell>
+          <Progressbar progress={(landpad.successful_landings / landpad.attempted_landings) * 100} color="green" />
+          {parseInt((landpad.successful_landings / landpad.attempted_landings) * 100)}
+        </TableBodyCell>
+        <TableBodyCell><a href={landpad.wikipedia}><LinkOutline /></a></TableBodyCell>
+        <TableBodyCell>{landpad.status}</TableBodyCell>
+      </TableBodyRow>
+    {/each}
+  </TableBody>
+</Table>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 import flowbitePlugin from 'flowbite/plugin'
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}'],
   darkMode: 'selector',
   theme: {
     extend: {
