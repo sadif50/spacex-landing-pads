77b90370e4fdf75ad347abd61e68f129972e8e66
S. M. Sujan <smsujon50@gmail.com>
2024-11-19 00:00:18 +0600

    data will show based on selected view type

--- src/lib/components/dataTable/DataViewController.svelte
+++ src/lib/components/dataTable/DataViewController.svelte
@@ -1,13 +1,22 @@
 <script>
   import DataFilter from "./DataFilter.svelte";
-import DataTable from "./DataTable.svelte";
+  import DataTable from "./DataTable.svelte";
   import TableViewButton from "./TableViewButton.svelte";
+
+  let tableType = $state('table');
+  const setTableType = (type) =>{
+    tableType = type;
+  }
 </script>
 
 <div>
   <div class="flex justify-between mb-[18px]">
-    <TableViewButton />
+    <TableViewButton {tableType} {setTableType} />
     <DataFilter />
   </div>
-  <DataTable />
+  {#if tableType === 'table'}
+    <DataTable />
+  {:else}
+    <h1>Grid Table here</h1>
+  {/if}
 </div>
--- src/lib/components/dataTable/TableViewButton.svelte
+++ src/lib/components/dataTable/TableViewButton.svelte
@@ -1,13 +1,15 @@
 <script>
   import { ButtonGroup, Button } from 'flowbite-svelte';
   import { UserCircleSolid, AdjustmentsVerticalOutline, DownloadSolid, ListOutline, GridOutline } from 'flowbite-svelte-icons';
+
+  let {tableType, setTableType} = $props();
 </script>
 
 <ButtonGroup>
-  <Button outline color="light">
+  <Button outline color="light" class={tableType === 'table'? 'bg-slate-50':''} onclick={()=>setTableType('table')}>
     <ListOutline class="w-4 h-4 me-2" />
   </Button>
-  <Button outline color="light">
+  <Button outline color="light" class={tableType === 'grid'? 'bg-slate-50':''} onclick={()=>setTableType('grid')}>
     <GridOutline class="w-4 h-4 me-2" />
   </Button>
 </ButtonGroup>
