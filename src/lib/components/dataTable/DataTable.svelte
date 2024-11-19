3d4a830e258407838d1059d384ce6aabcf33dfe1
S. M. Sujan <smsujon50@gmail.com>
2024-11-19 09:42:46 +0600

    added condition for data table success rate showing in progress bar

--- src/lib/components/dataTable/DataTable.svelte
+++ src/lib/components/dataTable/DataTable.svelte
@@ -24,8 +24,12 @@
         <TableBodyCell tdClass="p-4">{landpad.location.region}</TableBodyCell>
         <TableBodyCell tdClass="p-4"><Button size="xs" color="light">View Details</Button></TableBodyCell>
         <TableBodyCell tdClass="p-4">
-          <Progressbar progress={Math.round((landpad.successful_landings / landpad.attempted_landings) * 100)} progressClass="bg-[#31C48D]" />
-          {!isNaN((landpad.successful_landings / landpad.attempted_landings) * 100) ? Math.round((landpad.successful_landings / landpad.attempted_landings) * 100) + "%" : "NA"}
+          {#if !isNaN(landpad.successful_landings / landpad.attempted_landings)}
+            <Progressbar progress={Math.round((landpad.successful_landings / landpad.attempted_landings) * 100)} progressClass="bg-[#31C48D]" />
+            { Math.round((landpad.successful_landings / landpad.attempted_landings) * 100) + "%"}
+          {:else}
+            {"N/A"}
+          {/if}
         </TableBodyCell>
         <TableBodyCell tdClass="p-4"><a href={landpad.wikipedia} target="_blank"><LinkOutline color="#1C64F2" /></a></TableBodyCell>
         <TableBodyCell tdClass="p-4"><Badge color={(landpad.status == 'active') ? 'green' : (landpad.status === 'retired') ? 'red' : 'indigo'}>{landpad.status}</Badge></TableBodyCell>
